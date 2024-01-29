f7ffafff05b986c4834af544de13383787e025d7
JayDeep-Giri <jaydeepgiri30@gmail.com>
2024-01-29 21:44:22 +0530

    general layout for SignIn/LogIn

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,27 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ): ( 
+        <>
+          <img
+            src="assets/images/side-img.png"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  )
+}
+
+export default AuthLayout
